f9c3f0a3260c07e228dec2822c62821a8a834b25
Gabriel Krotkov <gabriel.krotkov@thinkesi.com>
2022-01-04 08:19:16 -0600

    restructured some nomenclature discovered issue with get_item_comment_body; only looping i = 1, 2 but returning correctly.

--- json_unpack_fxns.R
+++ json_unpack_fxns.R
@@ -2,9 +2,14 @@
 # Gabriel Krotkov
 # 12/1/2021
 
+#################
+#### LOADERS ####
+#################
+
 load_libraries <- function(){
   library(rjson)
   library(tidyverse)
+  library(data.table) # used to export to csvs quickly
 }
 
 load_data <- function(filepath){
@@ -12,6 +17,10 @@ load_data <- function(filepath){
                                   paste(readLines(filepath),collapse=",")))
   return(data)
 }
+
+#################
+#### GETTERS ####
+#################
 
 # returns the ticketID of one json object
 get_id <- function(item){
@@ -29,13 +38,13 @@ get_item_comments <- function(item){
 }
 
 # gets only the comment body of the item
-# designed to work with get_data_comments fxn
+# designed to work with get_comments fxn
 get_item_comment_body <- function(item){
   return(item$comments[[1]]$body)
 }
 
 # returns a list of all the comments in all the json objects
-get_data_comments <- function(data){
+get_comments <- function(data){
   result <- vector(mode = "list", length = length(data))
   for (i in 1:length(data)){
     # we suppress warnings since we're intentionally making a ragged list
@@ -45,16 +54,35 @@ get_data_comments <- function(data){
   return(result)
 }
 
-construct_data_frame <- function(data){
+#########################
+#### DF CONSTRUCTION ####
+#########################
+
+# construct a data frame from the json list including id and comments
+construct_comments_data_frame <- function(data){
   ids <- get_ids(data)
-  comments <- get_data_comments(data)
+  comments <- get_comments(data)
   result <- data.frame(id = ids)
   result$comments <- comments
   return(result)
 }
 
 
+# generic data frame construction
+# we always get ids since those are the unique field.
+construct_data_frame <- function(data, fields = "comments"){
+  ids <- get_ids(data)
+  
+}
+
+################
+#### EXPORT ####
+################
+
+fast_export <- function(data){
+  # first, detect all NULLS and convert to empty (fwrite can't handle NULL)
+  # @TODO
+}
+
 # @TODO only include comment body in comment column
 # access token: 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
-
-# making some example changes
