02edcba0c3671cbdb32eeb9d95d3660130bef034
Gabriel Krotkov <gabriel.krotkov@thinkesi.com>
2022-02-01 22:30:33 -0600

    Updated parser.py to include more of the R script's functionality and also just run in one click.

--- parser.py
+++ parser.py
@@ -19,7 +19,6 @@ def clear_dir(dir):
         os.remove(os.path.join(dir, f))
 
 # non-pandas line by line reading of a json line separated file in utf8 encoding
-# 
 def read_jl_lines(filepath):
     file = open(filepath, "r", encoding = "utf8")
     lines = []
@@ -27,29 +26,50 @@ def read_jl_lines(filepath):
         lines.append(json.loads(line))
     file.close()
 
-#####################
-#### Driver Code ####
-#####################
+# colnames must be a list of strings, legal column names for the pandas 
+# dataframe data
+def get_columns(data, colnames):
+    return data[colnames]
+
+# wrapper function for pandas syntax, gets column names of a dataframe
+def get_colnames(data):
+    return list(data.columns)
+
+def export(data, name = "output", target = None):
+    if (target == None):
+        target = f"data/parser_output/{name}.json"
+    data.to_json(target, orient = "records", 
+                 force_ascii = False, lines = True)
 
 # The user must define how many splits they want in the data. 
 # The program will evenly divide the number of json objects in the input file
 # but will not account for varying size of json objects.
-splits = int(input("How many splits?"))
-message = "If you don't need to split the data, don't run this script."
-assert(splits > 1, message)
-filepath = "data/full zendesk 042020.json"
+def split_data(data, splits):
+    clear_dir("data/parser_output")
+    assert(splits > 1, "Need splits > 1")
+    split_length = len(data) // splits
+    # send split data to data/splits
+    for i in range(splits):
+        tmp = data[(split_length * i):(split_length * (i + 1))]
+        if (i == splits - 1): # if last iteration, take all the remaining data
+            tmp = data[(split_length * 1):]
+        target = f"data/parser_output/split{i + 1}.json"
+        tmp.to_json(target, orient = "records", 
+                    force_ascii = False, lines = True)
 
+# splits should be the # of splits desired if provided
+def export_comments(data, splits = False):
+    # select down the data to only these columns
+    data = get_columns(data, ["id", "comments"])
+    if(splits):
+        split_data(data, splits)
+    else:
+        export(data, "comments")
+
+#####################
+#### Driver Code ####
+#####################
+# can adjust this to default to looping through all files in parser_input?
+filepath = "data/parser_input/" + os.listdir("data/parser_input")[0]
 data = pd.read_json(filepath, lines=True)
-
-clear_dir("data/splits")
-
-# compute the length required for each of the split dataframes.
-# The mod solution presented here is not perfect but is only separated from 
-# perfect by (splits - 1) json objects, which is negligible on this scale.
-split_length = len(data) // splits
-for i in range(splits):
-    tmp = data[(split_length * i):(split_length * (i + 1))]
-    if (i == splits - 1): # if last iteration, take all the remaining data
-        tmp = data[(split_length * 1):]
-    target = f"data/splits/split{i + 1}.json"
-    tmp.to_json(target, orient = "records", force_ascii = False, lines = True)
+export_comments(data)
